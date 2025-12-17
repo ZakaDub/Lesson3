dcd434a50124b0d0d90989c7164a608b87538ebc
testName <zakdubr@gmail.com>
2025-12-17 14:22:30 +0200

    homeworkLesson3

---
+++ Lesson3/index.js
@@ -0,0 +1,12 @@
+const pinCodes = ["1111", "5678", "1234", "9999"];
+
+const myPin = "1234"
+
+for (pinCode of pinCodes) {
+    if(pinCode === myPin) {
+        console.log("Пин код найден!")
+        continue
+    } 
+    
+    else {console.log("Неверный пин..")};
+}
